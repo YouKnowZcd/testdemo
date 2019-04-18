82e553ed216ac6a0f48d644239d867ef9bdf81e1
Xoron <xoron@vatademacbook-pro.local>
2019-04-18 09:27:38 +0800

    Initial Commit

---
+++ testtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testtest
+//
+//  Created by Xoron on 2019/4/18.
+//  Copyright © 2019年 Xoron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testtest
+//
+//  Created by Xoron on 2019/4/18.
+//  Copyright © 2019年 Xoron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
