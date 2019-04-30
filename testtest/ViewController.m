34088eda362e367d31c8da93b8fda6a09c5227be
zhangchen <zhangchen828119@163.com>
2019-04-30 09:42:07 +0800

    注释语句

--- testtest/ViewController.m
+++ testtest/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
@@ -16,8 +17,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //[self getUpdatemMessage];
+    
+    
+    UIWebView *view = [[UIWebView alloc]initWithFrame:self.view.frame];
+    view.backgroundColor = [UIColor yellowColor];
+
+   // NSString *url = @"http://www.baidu.com";
+    NSString *url = @"http://esb.sucdri.com:8037/dev.CJY.WebApp/index.html?uname=0144&pwd=123456&uuid=E1573B35-94FE-4BB1-833D-67D3816E330D";
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];;
+    [view loadRequest:request];
+    [self.view addSubview:view];
+    
 }
-
-
 @end
